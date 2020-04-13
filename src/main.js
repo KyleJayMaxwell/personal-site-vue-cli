b19f22af6538c55a32d4fd5b2eb98fc85b9969b9
Kial Maxwell <KyleJMaxwell@Kials-2014-MacBook-Pro.local>
2020-04-12 23:28:22 -0700

    original change for vue cli personal site

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
+
+library.add(faLinkedin, faGithub, faInstagram);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
